5d86f760034198b0fe7e53d9f2a4f014068a3304
MZDisaster <moaazw@gmail.com>
2016-11-22 16:01:15 +0100

    Created Garage 2.5

---
+++ Garage 2.5.1/Models/VehicleViews/VehicleEditViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._5.Models
+{
+    public class VehicleEditModel
+    {
+        [Required]
+        [MinLength(0)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(6),MaxLength(32)]
+        public string Owner { get; set; }
+
+        [Required]
+        [RegularExpression("/^[A-Z]{3}[0-9]{3}/")]
+        public string RegNr { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(VehicleTypes))]
+        public VehicleTypes VType { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(Colors))]
+        public Colors Color { get; set; }
+
+        [Required]
+        [Range(0,18)]
+        public int wheels { get; set; }
+    }
+}
---
+++ Garage 2.5.1/Models/VehicleViews/VehicleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._5.Models
+{
+    public class VehicleViewModel
+    {
+        public string Owner { get; set; }
+
+        public string RegNr { get; set; }
+
+        public VehicleTypes VType { get; set; }
+
+        public Colors Color { get; set; }
+
+        public int wheels { get; set; }
+    }
+}
