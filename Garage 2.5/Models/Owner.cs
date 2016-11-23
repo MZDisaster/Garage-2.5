d730e92e5d82856077eb327fe105c6368f1d2a17
MZDisaster <moaazw@gmail.com>
2016-11-23 14:28:24 +0100

    Created Garage 2.5

---
+++ Garage 2.5/Models/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._5.Models
+{
+    public class Owner
+    {
+        [Key]
+        public int PNR { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Garage 2.5/Models/VehicleType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage_2._5.Models
+{
+    public class VehicleType
+    {
+        [Key]
+        public int TypeId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
