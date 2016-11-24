4f97f207a252143d8d0ef150e2cb2292ff7fb1ea
KlasClaywood <klas.claywood@gmail.com>
2016-11-24 10:48:45 +0100

    Dropdownlist through viewbag

--- Garage 2.5/Controllers/HomeController.cs
+++ Garage 2.5/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Garage_2._5.Repositories;
+﻿using Garage_2._5.Models;
+using Garage_2._5.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,23 @@ namespace Garage_2._5.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            List<SelectListItem> types = new List<SelectListItem>();
+            foreach (VehicleType type in Repo.GContext.VehicleTypes)
+            {
+                types.Add(new SelectListItem() { Text = type.Name, Value = type.TypeId.ToString() });
+            }
+            ViewBag.VehicleTypes = types;
+            List<SelectListItem> owners = new List<SelectListItem>();
+            foreach (Owner owner in Repo.GContext.Owners)
+            {
+                owners.Add(new SelectListItem() { Text = owner.Name, Value = owner.PNR });
+            }
+            ViewBag.Owners = owners;
+            return View(new Vehicle());
         }
     }
 }
